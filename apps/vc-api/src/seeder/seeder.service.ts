2ac76af6917a1ad4b71354e7c61668a32b61ecdf
ShikharBhatt <shikhar.bhatt@shell.com>
2024-08-25 23:04:21 +0530

    feat: integrate credo in key and seeder module

--- apps/vc-api/src/seeder/seeder.service.ts
+++ apps/vc-api/src/seeder/seeder.service.ts
@@ -4,18 +4,16 @@
  */
 
 import { Injectable, Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { KeyPair } from '../key/key-pair.entity';
-import { Repository } from 'typeorm';
 import { keyPairFixture } from './fixtures/key-pair.fixture';
 import { DIDService } from '../did/did.service';
+import { KeyService } from '../key/key.service';
 
 @Injectable()
 export class SeederService {
   private readonly logger = new Logger(SeederService.name, { timestamp: true });
 
   constructor(
-    @InjectRepository(KeyPair) private readonly keyPairRepository: Repository<KeyPair>,
+    private readonly keyService: KeyService,
     private readonly didService: DIDService
   ) {}
 
@@ -23,8 +21,8 @@ export class SeederService {
     this.logger.debug('seeding database');
 
     for (const keyPair of keyPairFixture) {
-      await this.keyPairRepository.save(keyPair);
-      await this.didService.registerKeyDID(keyPair.publicKeyThumbprint);
+      const key = await this.keyService.importKey(keyPair);
+      await this.didService.registerKeyDID(key.keyId);
     }
 
     this.logger.debug('seeding database complete');
