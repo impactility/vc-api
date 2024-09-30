47d6352b86ea2a23ed4598df23088e269800250b
ShikharBhatt <shikhar.bhatt@impactility.com>
2024-09-30 10:32:44 +0530

    chore: update db paths
    Signed-off-by: ShikharBhatt <shikhar.bhatt@impactility.com>

--- apps/vc-api/src/db-config.ts
+++ apps/vc-api/src/db-config.ts
@@ -4,20 +4,24 @@
  */
 
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { homedir } from 'os';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 /**
  * Inspired by https://dev.to/webeleon/unit-testing-nestjs-with-typeorm-in-memory-l6m
  * @param inMemory // default set to false; set to true for unit testing  
  */
 export const TypeOrmSQLiteModule = (inMemory=false) =>
-  TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: !inMemory ? 
-      `${homedir}/.afj/data/wallet/${process.env['ASKAR_WALLET_ID']}/sqlite.db` // path to askar wallet db
-      : ':memory:',
-    // dropSchema: true, // disabling this option to avoid dropping askar tables
-    autoLoadEntities: true, // https://docs.nestjs.com/techniques/database#auto-load-entities
-    // synchronize: true, // disabling this option to avoid losing existing data
-    keepConnectionAlive: true // https://github.com/nestjs/typeorm/issues/61
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      type: 'sqlite',
+      database: !inMemory ? 
+        `${configService.get<string>('DB_BASE_PATH')}/exchanges.db`
+        : ':memory:',
+      // dropSchema: true, // disabling this option to avoid dropping askar tables
+      autoLoadEntities: true, // https://docs.nestjs.com/techniques/database#auto-load-entities
+      synchronize: true,
+      keepConnectionAlive: true // https://github.com/nestjs/typeorm/issues/61
+    }),
+    inject: [ConfigService]
   });
