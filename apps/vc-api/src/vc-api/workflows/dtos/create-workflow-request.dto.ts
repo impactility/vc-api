d370beb206566b2f097c6e5433412663d7011f9b
jrhender <john.henderson@energyweb.org>
2024-10-23 15:29:38 -0700

    style: prettier on workflow files
    Signed-off-by: jrhender <john.henderson@energyweb.org>

--- apps/vc-api/src/vc-api/workflows/dtos/create-workflow-request.dto.ts
+++ apps/vc-api/src/vc-api/workflows/dtos/create-workflow-request.dto.ts
@@ -4,38 +4,38 @@ import { Type } from 'class-transformer';
 import { WorkflowStepDto } from './workflow-step.dto';
 
 export class Steps {
-    [key: string]: WorkflowStepDto;
+  [key: string]: WorkflowStepDto;
 }
 
 export class WorkflowConfig {
-    @ApiProperty({
-      description: 'One or more steps required to complete an exchange on the workflow.',
-      type: Steps
-    })
-    @ValidateNested()
-    @Type(() => Steps)
-    steps: Steps;
+  @ApiProperty({
+    description: 'One or more steps required to complete an exchange on the workflow.',
+    type: Steps
+  })
+  @ValidateNested()
+  @Type(() => Steps)
+  steps: Steps;
 
-    @ApiProperty({
-      description: 'The initial step of the workflow.'
-    })
-    @IsString()
-    initialStep: string;
+  @ApiProperty({
+    description: 'The initial step of the workflow.'
+  })
+  @IsString()
+  initialStep: string;
 
-    @ApiProperty({
-      description: 'The ID that will be used for the created workflow. Passing an ID is OPTIONAL.'
-    })
-    @IsString()
-    @IsOptional()
-    id: string;
+  @ApiProperty({
+    description: 'The ID that will be used for the created workflow. Passing an ID is OPTIONAL.'
+  })
+  @IsString()
+  @IsOptional()
+  id: string;
 }
 
 export class CreateWorkflowRequestDto {
-    @ApiProperty({
-      description: 'Configuration for a workflow',
-      type: WorkflowConfig
-    })
-    @ValidateNested()
-    @Type(() => WorkflowConfig)
-    config: WorkflowConfig;
+  @ApiProperty({
+    description: 'Configuration for a workflow',
+    type: WorkflowConfig
+  })
+  @ValidateNested()
+  @Type(() => WorkflowConfig)
+  config: WorkflowConfig;
 }
