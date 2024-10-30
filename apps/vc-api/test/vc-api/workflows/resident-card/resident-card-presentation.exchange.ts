40bfe3bf23622c4d2740a1142ccd57ddf15325a9
jrhender <john.henderson@energyweb.org>
2024-10-29 20:51:34 -0700

    test(workflows): copy resident-card exchange test to workflows
    Signed-off-by: jrhender <john.henderson@energyweb.org>

---
+++ apps/vc-api/test/vc-api/workflows/resident-card/resident-card-presentation.exchange.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 - 2023 Energy Web Foundation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { plainToClass } from 'class-transformer';
+import { ExchangeDefinitionDto } from '../../../../src/vc-api/exchanges/dtos/exchange-definition.dto';
+import { VpRequestInteractServiceType } from '../../../../src/vc-api/exchanges/types/vp-request-interact-service-type';
+import { VpRequestQueryType } from '../../../../src/vc-api/exchanges/types/vp-request-query-type';
+
+export class ResidentCardPresentation {
+  #exchangeId = `b229a18f-db45-4b33-8d36-25d442467bab`;
+  #callbackUrl: string;
+  queryType = VpRequestQueryType.presentationDefinition;
+
+  constructor(callbackUrl: string) {
+    this.#callbackUrl = callbackUrl;
+  }
+
+  getExchangeId(): string {
+    return this.#exchangeId;
+  }
+
+  getExchangeDefinition(): ExchangeDefinitionDto {
+    const exchangeDefinition: ExchangeDefinitionDto = {
+      exchangeId: this.#exchangeId,
+      query: [
+        {
+          type: this.queryType,
+          credentialQuery: [
+            {
+              presentationDefinition: {
+                id: '286bc1e0-f1bd-488a-a873-8d71be3c690e',
+                input_descriptors: [
+                  {
+                    id: 'permanent_resident_card',
+                    name: 'Permanent Resident Card',
+                    purpose: 'We can only allow permanent residents into the application',
+                    constraints: {
+                      fields: [
+                        {
+                          path: ['$.type'],
+                          filter: {
+                            type: 'array',
+                            contains: {
+                              type: 'string',
+                              const: 'PermanentResidentCard'
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ],
+      interactServices: [
+        {
+          type: VpRequestInteractServiceType.unmediatedPresentation
+        }
+      ],
+      isOneTime: true,
+      callback: [
+        {
+          url: this.#callbackUrl
+        }
+      ]
+    };
+    return plainToClass(ExchangeDefinitionDto, exchangeDefinition);
+  }
+}
