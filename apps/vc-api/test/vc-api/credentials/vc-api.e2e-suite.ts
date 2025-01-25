7b324612128f46392e441955c9fd774446d18889
John Henderson <jrhender@users.noreply.github.com>
2025-01-25 05:06:57 +0000

    test(issue e2e): issuance with remote context (#44)
    Updated nock as well as some issues with document loading calls.
    
    Signed-off-by: jrhender <john.henderson@energyweb.org>

--- apps/vc-api/test/vc-api/credentials/vc-api.e2e-suite.ts
+++ apps/vc-api/test/vc-api/credentials/vc-api.e2e-suite.ts
@@ -3,14 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as nock from 'nock';
 import * as request from 'supertest';
 import { CredentialDto } from '../../../src/vc-api/credentials/dtos/credential.dto';
 import { IssueOptionsDto } from '../../../src/vc-api/credentials/dtos/issue-options.dto';
 import { app, walletClient } from '../../app.e2e-spec';
 import { API_DEFAULT_VERSION_PREFIX } from '../../../src/setup';
+import { VerifyCredentialDto } from '../../../src/vc-api/credentials/dtos/verify-credential.dto';
 
 export const vcApiSuite = () => {
-  it('should issue using a generated did:key', async () => {
+  it('should issue simple credential using a generated did:key', async () => {
     const didDoc = await walletClient.createDID('key');
     const credential: CredentialDto = {
       '@context': ['https://www.w3.org/2018/credentials/v1'],
@@ -28,5 +30,50 @@ export const vcApiSuite = () => {
       .send({ credential, options })
       .expect(201);
     expect(postResponse.body).toBeDefined();
+  });
+
+  it('should issue credential with remote context using a generated did:key', async () => {
+    const didDoc = await walletClient.createDID('key');
+    const remoteContextBase = 'https://example.com';
+    const remoteContextPath = '/remote-ld-context';
+    const remoteContext = {
+      '@context': {
+        '@version': 1.1,
+        '@protected': true,
+        examplebase: `${remoteContextBase}${remoteContextPath}#`,
+        consent: 'examplebase:consent',
+        ConsentCredential: 'examplebase:ConsentCredential'
+      }
+    };
+    const contextLoadScope = nock(remoteContextBase)
+      .persist()
+      .get(remoteContextPath)
+      .reply(200, remoteContext);
+
+    const credential: CredentialDto = {
+      '@context': ['https://www.w3.org/2018/credentials/v1', `${remoteContextBase}${remoteContextPath}`],
+      id: 'urn:uuid:49f69fb8-f256-4b2e-b15d-c7ebec3a507e',
+      type: ['VerifiableCredential', 'ConsentCredential'],
+      credentialSubject: {
+        consent: 'I consent to such and such',
+        id: 'did:key:z6MkfGg96cNEL2Ne4z9HD3BSQhhD2neZKTzyE1y5wUu9KM4h'
+      },
+      issuer: didDoc.id,
+      issuanceDate: '2022-10-03T12:19:52Z'
+    };
+    const issueOptions: IssueOptionsDto = {};
+    const issueResponse = await request(app.getHttpServer())
+      .post(`${API_DEFAULT_VERSION_PREFIX}/vc-api/credentials/issue`)
+      .send({ credential, options: issueOptions })
+      .expect(201);
+    expect(issueResponse.body).toBeDefined();
+    contextLoadScope.done();
+
+    const verifiableCredential = issueResponse.body;
+    const verifyRequestBody: VerifyCredentialDto = { verifiableCredential };
+    await request(app.getHttpServer())
+      .post(`${API_DEFAULT_VERSION_PREFIX}/vc-api/credentials/verify`)
+      .send(verifyRequestBody)
+      .expect(200);
   });
 };
